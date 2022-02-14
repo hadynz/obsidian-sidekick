836f2adfeb63a02c48821d7a4c9592d7f5bd2051
Hady Osman <hadyos@gmail.com>
2022-02-14 23:34:52 +1300

    Fix ignoring of YAML frontmatter
    Correctly handle the two valid frontmatter scenarios:
    
    ```
    ---
    yaml: "with no empty lines preceding it"
    ---
    ```
    
    and
    
    ```
    
    ---
    yaml: "with empty lines preceding it"
    ---
    ```

--- src/search/index.ts
+++ src/search/index.ts
@@ -37,7 +37,7 @@ export default class Search {
   private redactText(text: string): string {
     return text
       .replace(/```[\s\S]+?```/g, (m) => ' '.repeat(m.length)) // remove code blocks
-      .replace(/^\n+---[\s\S]+?---/g, (m) => ' '.repeat(m.length)) // remove yaml front matter
+      .replace(/^\n*?---[\s\S]+?---/g, (m) => ' '.repeat(m.length)) // remove yaml front matter
       .replace(/#+([a-zA-Z0-9_]+)/g, (m) => ' '.repeat(m.length)) // remove hashtags
       .replace(/\[(.*?)\]+/g, (m) => ' '.repeat(m.length)); // remove links
   }
